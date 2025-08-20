d4e944df47cca06e9cc32f575c01aff06d75ed25
Lucas Sorzio <sorziolucas4@gmail.com>
2025-08-20 14:33:37 -0300

    arreglo cuando no se encuentra una foto

--- app-peli/src/Componentes/Pelicula.jsx
+++ app-peli/src/Componentes/Pelicula.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import logo from '../assets/logo.jpg'
 import { Link } from 'react-router-dom'
 
-const Pelicula = ({id,titulo,imagen}) => {
+const Pelicula = ({ id, titulo, imagen }) => {
+  const poster = imagen 
+    ? `https://image.tmdb.org/t/p/w500/${imagen}` 
+    : logo;
+
   return (
-      <div className='transition-transform duration-300 hover:scale-105 hover:opacity-80 cursor-pointer hover:bg-blue-950 hover:border-blue-500 hover:border-2 flex flex-col items-center h-fit w-fit just m-2 p-2 bg-gray-900 rounded-lg shadow-lg'>
-          
+    <div className=" w-fit h-fit transition-transform duration-300 hover:scale-105 hover:opacity-80 cursor-pointer hover:bg-blue-950 hover:border-blue-500 hover:border-2 flex flex-col items-center  m-2 p-4 bg-gray-900 rounded-lg shadow-lg">
+      
+      <p className="text-lg font-extrabold text-amber-200 mb-2 text-center">
+        {titulo}
+      </p>
+      
+      <img
+        className="w-60 h-80 object-cover rounded-2xl border border-blue-400 shadow-md"
+        src={poster}
+        alt={titulo}
+      />
 
-      <p className='text-1xl font-extrabold '><b>{titulo }</b></p>
-          <img className='w-60  rounded-2xl border-blue-400 border-1' src={`https://image.tmdb.org/t/p/w500/${imagen}`} alt={titulo} />
-
-          <Link to={`/detalles/${id}`} className='bg-blue-950 p-2 m-2 rounded-xl text-amber- hover:bg-blue-400 font-bold hover:border-1' >Ver Detalles</Link>
+      <Link
+        to={`/detalles/${id}`}
+        className="bg-blue-950 p-2 mt-3 rounded-xl text-amber-200 font-bold border border-transparent hover:bg-blue-400 hover:text-gray-900 hover:border-blue-700 transition-colors duration-300"
+      >
+        Ver Detalles
+      </Link>
     </div>
   )
 }
