2c80ec31688faaf669ec4682b3bd2c1745616ccc
Lucas Sorzio <sorziolucas4@gmail.com>
2025-12-11 15:03:16 -0300

    mejora estilos

--- app-peli/src/Componentes/Navbar.jsx
+++ app-peli/src/Componentes/Navbar.jsx
@@ -5,20 +5,20 @@ import logo from '../assets/logo.jpg'
   return (
       <div>
           
-         <nav className='flex flex-col md:flex-row md:items-center w-screen justify-between  shadow-lg p-4'>
+         <nav className='flex flex-col bg-black md:flex-row md:items-center w-screen justify-between  shadow-lg p-4'>
              
               
                   <Link  to="/">
                 <img className='w-15 border-blue-800 border rounded-2xl hover:bg-blue-900 hover:border-blue-400' src={logo}></img>
                   </Link>
                     <Link  to="/buscar">
-                  <button className='p-4  text-amber-200 text-2xl font-semibold   hover:bg-blue-400 hover:text-md hover:text-blue-900 '>Buscar</button>
+                  <button className='p-4  text-amber-200 md:text-2xl font-semibold   hover:bg-blue-400 hover:text-md hover:text-blue-900 '>Buscar</button>
         </Link>
         <Link  to="/favoritos">
-                  <button className='p-4 text-amber-200 text-2xl font-semibold border-b-blue-900  hover:bg-blue-400 hover:text-md hover:text-blue-900 '>Favoritos</button>
+                  <button className='p-4 text-amber-200 md:text-2xl  font-semibold border-b-blue-900  hover:bg-blue-400 hover:text-md hover:text-blue-900 '>Favoritos</button>
               </Link>
                 <Link to="/contacto">
-                   <button className='p-4 text-amber-200 text-2xl font-semibold border-b-blue-900 hover:bg-blue-400 hover:text-md hover:text-blue-900 '>Contacto</button>
+                   <button className='p-4 text-amber-200 md:text-2xl  font-semibold border-b-blue-900 hover:bg-blue-400 hover:text-md hover:text-blue-900 '>Contacto</button>
                   </Link>
               
         </nav>
